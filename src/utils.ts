8eeafe1d1b248e2a0097d867a3699d7be07c443c
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2024-04-11 23:29:50 +0100

    playlistitem component

---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const formatDuration = (durationInSeconds: number): string => {
+    const minutes: number = Math.floor(durationInSeconds / 60);
+    const seconds: number = durationInSeconds % 60;
+    return minutes.toString() + ':' + seconds.toString().padStart(2, '0')
+}
